35f017dd4f4d3f9dda939b7ccd79ec1d5d2e1aec
cluthere <69466884+cluthere@users.noreply.github.com>
2023-12-02 12:49:40 +0530

    nothing

--- src/components/product/Card.jsx
+++ src/components/product/Card.jsx
@@ -41,6 +41,7 @@ export default function Card() {
             alt={item.name}
             className="z-0 h-full w-full rounded-md object-cover"
           />
+          
           <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
           <div className="absolute bottom-4 left-4 text-left">
             <h1 className="text-lg font-semibold text-white">{item.name}</h1>
